fe7c1ea81ce84b83df7fdc7a13a371e4d2627f97
Dron-Programador <david.nicolas1352@gmail.com>
2025-06-17 13:27:46 -0600

    cambio

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ print("Hello, World!");
 // This is a simple JavaScript program that prints "Hello, World!" to the console.
 
 // como andan mi people (mensaje de bredny)
+
+//al millon mi Bredny y usted 
+// hay que eperar al lenton de Alan 
